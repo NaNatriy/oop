d7295de5e8ad34ceecc294f51cce2904bc8035fa
loses <loses626@gmail.com>
2022-12-19 15:53:12 +0600

    3 Задание

--- src/oop/Car.java
+++ src/oop/Car.java
@@ -17,9 +17,24 @@ public class Car {
         this.country = country;
     }
 
+    Car() {
+        this.brand = "default";
+        this.model = "default";
+        this.color = "белый";
+        this.country = "default";
+
+        if (engineVolume <= 0) {
+            engineVolume = 1.5;
+        }
+        if (year <= 0) {
+            year = 2000;
+        }
+    }
 
     @Override
     public String toString() {
         return  brand + " " + model + ", " + year + " год выпуска, сборка " + country  + ", " + color + " цвет, объем двигателя - " + engineVolume;
     }
+
+
 }
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Car audi = new Car("Audi", "A8 50L TDI quattro", 3.0, "Черный", 2020, "Германия");
         Car bmw = new Car("BMW", "Z8", 3.0, "Черный", 2021, "Германия");
         Car kia = new Car("Kia", "Sportage 4", 2.4, "красный", 2018, "Южная Корея");
-        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        Car hyundai = new Car();
 
 
         System.out.println(lada);
