a5b482e90c64eb4ebd708649bb527975b9e91ca5
loses <loses626@gmail.com>
2022-12-19 15:32:19 +0600

    2 Задание

--- src/oop/Car.java
+++ src/oop/Car.java
@@ -8,6 +8,16 @@ public class Car {
     int year;
     String country;
 
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+
     @Override
     public String toString() {
         return  brand + " " + model + ", " + year + " год выпуска, сборка " + country  + ", " + color + " цвет, объем двигателя - " + engineVolume;
