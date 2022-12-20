c00b68cc8a8f04cf28e07c6f7acd6a561fdb6464
loses <loses626@gmail.com>
2022-12-20 16:41:53 +0600

    Доделал

--- src/oop/Car.java
+++ src/oop/Car.java
@@ -1,34 +1,52 @@
 package oop;
-
 public class Car {
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int year;
-    String country;
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
 
     public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (country == null) {
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
+        if (color == null) {
+            this.country = "белый";
+        } else {
+            this.color = color;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (year == 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
     }
 
-    Car() {
+    public Car() {
         this.brand = "default";
         this.model = "default";
         this.color = "белый";
         this.country = "default";
-
-        if (engineVolume <= 0) {
-            engineVolume = 1.5;
-        }
-        if (year <= 0) {
-            year = 2000;
-        }
+        this.engineVolume = 1.5;
+        this.year = 2000;
     }
 
     @Override
